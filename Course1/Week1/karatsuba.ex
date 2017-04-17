1cfeb463003e9b5f8c04173f93fffa8e92079723
sunilffx <sunil.x.gopinath@gmail.com>
2017-04-17 10:41:38 +1000

    easier way to split numbers

--- Course1/Week1/karatsuba.ex
+++ Course1/Week1/karatsuba.ex
@@ -16,26 +16,23 @@ defmodule Karatsuba do
     # let d = second half of y
     # where e  = (a + b) * (c + d) - a1 - d1
     # result = a * 10^n + e * 10^n/2 + d
-    x0 = Integer.digits(x)
-    y0 = Integer.digits(y)
-    n = x0 |> length
-    a = x0 |> split_first(n)
-    b = x0 |> split_second(n)
-    c = y0 |> split_first(n)
-    d = y0 |> split_second(n)
+    n = Integer.digits(x) |> length
+    a = split_first(x, div(n, 2))
+    b = split_second(x, div(n, 2))
+    c = split_first(y, div(n, 2))
+    d = split_second(y, div(n, 2))
     a1 = calculate_product(a,c)
     d1 = calculate_product(b,d)
     e1 = calculate_product((a+b),(c+d)) - a1 - d1
     a1 * :math.pow(10,n) + e1 * :math.pow(10,(div(n,2))) + d1
   end
 
-  defp split_first(digits, len) do
-    digits
-    |> Enum.take(div(len,2)) |> Enum.join |> String.to_integer
+  defp split_first(x, len) do
+    div(x, round(:math.pow(10, len)))
+
   end
 
-  defp split_second(digits, len) do
-    digits
-    |> Enum.take(-div(len,2)) |> Enum.join |> String.to_integer
+  defp split_second(x, len) do
+    rem(x, round(:math.pow(10, len)))
   end
 end
