90b4080dc8387f690441f10e648ab637b8d9730f
Sunil Gopinath <sxg282@gmail.com>
2017-08-14 01:27:28 +1000

    first commit of is_bst_hard
    Adapting the bst algorithm from wiki

---
+++ DataStructures/Week4/is_bst_hard.java
@@ -0,0 +1,83 @@
+import java.util.*;
+import java.io.*;
+
+public class is_bst_hard {
+    class FastScanner {
+        StringTokenizer tok = new StringTokenizer("");
+        BufferedReader in;
+
+        FastScanner() {
+            in = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (!tok.hasMoreElements())
+                tok = new StringTokenizer(in.readLine());
+            return tok.nextToken();
+        }
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+    }
+
+    public class IsBST {
+        class Node {
+            int key;
+            int left;
+            int right;
+
+            Node(int key, int left, int right) {
+                this.left = left;
+                this.right = right;
+                this.key = key;
+            }
+        }
+
+        int nodes;
+        Node[] tree;
+
+        void read() throws IOException {
+            FastScanner in = new FastScanner();
+            nodes = in.nextInt();
+            tree = new Node[nodes];
+            for (int i = 0; i < nodes; i++) {
+                tree[i] = new Node(in.nextInt(), in.nextInt(), in.nextInt());
+            }
+        }
+
+        boolean isBinarySearchTree() {
+          // Implement correct algorithm here
+          List<Integer> list = new ArrayList<Integer>();
+          if(tree.length < 1) {
+            return true;
+          }
+          return isBinarySearchTree(0, -2147483650L, 2147483650L);
+        }
+        // This is an adaptation of the algorithm from wiki
+        boolean isBinarySearchTree(int index, long minKey, long maxKey) {
+          if(index == -1) return true;
+          if(tree[index].key < minKey || tree[index].key >= maxKey) return false;
+          return isBinarySearchTree(tree[index].left, minKey, tree[index].key) && isBinarySearchTree(tree[index].right, tree[index].key, maxKey);
+        }
+    }
+
+    static public void main(String[] args) throws IOException {
+        new Thread(null, new Runnable() {
+            public void run() {
+                try {
+                    new is_bst_hard().run();
+                } catch (IOException e) {
+                }
+            }
+        }, "1", 1 << 26).start();
+    }
+    public void run() throws IOException {
+        IsBST tree = new IsBST();
+        tree.read();
+        if (tree.isBinarySearchTree()) {
+            System.out.println("CORRECT");
+        } else {
+            System.out.println("INCORRECT");
+        }
+    }
+}
